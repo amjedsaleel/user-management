af1e00baf7d1a44c94e6edb2b6028fe4cb13ef6e
Amjed Saleel K <amjedsaleel@gmail.com>
2021-10-22 11:34:27 +0530

    User login function

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 # Django
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # local Django
 from . forms import CustomUserCreationForm
@@ -8,6 +9,13 @@ from . forms import CustomUserCreationForm
 
 
 def login_fun(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
     return render(request, 'accounts/login.html')
 
 
