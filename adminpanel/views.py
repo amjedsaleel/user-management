078c09305ca5a8f5b243f529e814051bac2b7be0
Amjed Saleel K <amjedsaleel@gmail.com>
2021-10-23 19:24:20 +0530

    Users data displayed

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,7 +1,15 @@
+# Django
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required
 def dashboard(request):
-    return render(request, 'admin-panel/dashboard.html')
+    users = User.objects.all()
+    context = {
+        'users': users
+    }
+    return render(request, 'admin-panel/dashboard.html', context)
